534265dadb0e59f3984bc0f0dd529fbb4b9de27a
zzc <2272713550@qq.com>
2016-10-18 17:41:47 +0800

    basic

--- js/util/common.js
+++ js/util/common.js
@@ -12,3 +12,20 @@ function isTel(tel) {
         return false;
     };
 }
+
+// ps:注意将同名的放在一个数组里
+function getFormJson(form) {
+    var o = {};
+    var a = $(form).serializeArray();
+    $.each(a, function () {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+}
